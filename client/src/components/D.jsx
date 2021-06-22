7e1b811f2e314c5545b28c8a20cd02db8646103d
Rahul Chauhan <rahuljungchauhan@gmail.com>
2021-06-21 22:37:55 -0400

    Delete comments

--- client/src/components/D.jsx
+++ client/src/components/D.jsx
@@ -77,19 +77,6 @@ export default function CustomizedDialogs({ reviews }) {
           Reviews
         </DialogTitle>
         <DialogContent dividers>
-          {/* <Typography gutterBottom>
-            Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis
-            in, egestas eget quam. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
-          </Typography>
-          <Typography gutterBottom>
-            Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Vivamus sagittis
-            lacus vel augue laoreet rutrum faucibus dolor auctor.
-          </Typography>
-          <Typography gutterBottom>
-            Aenean lacinia bibendum nulla sed consectetur. Praesent commodo cursus magna, vel
-            scelerisque nisl consectetur et. Donec sed odio dui. Donec ullamcorper nulla non metus
-            auctor fringilla.
-          </Typography> */}
           <Reviews reviews={reviews} />
         </DialogContent>
         <DialogActions>
