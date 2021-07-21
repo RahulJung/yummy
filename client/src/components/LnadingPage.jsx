a5e385fcd9ee034bbc24a3438e9b1bfdf8711a67
RahulJung <rahuljungchauhan@gmail.com>
2021-07-21 13:30:46 -0400

    - Create a landing page

---
+++ client/src/components/LnadingPage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+class LandingPage extends React.Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+
+  render() {
+    return <div>Hello!!! this is landing page</div>;
+  }
+}
+
+export default LandingPage;
